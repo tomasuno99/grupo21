4d828765f14973b31c141294746601a2ea272e44
Manu <38946479+manurtinez@users.noreply.github.com>
2019-05-05 18:23:40 -0300

    agrego cositas

--- HWH/HomeSwitchHome/apps/residencia/urls.py
+++ HWH/HomeSwitchHome/apps/residencia/urls.py
@@ -4,4 +4,5 @@ from apps.residencia import views
 urlpatterns = [
     path('residencia/', views.product_detail),
     path('residencia/<id>', views.prueba, name='prue'),
+    path('agregar_residencia/', views.agregar_residencia, name='agregar'),
 ]
--- HWH/HomeSwitchHome/apps/residencia/views.py
+++ HWH/HomeSwitchHome/apps/residencia/views.py
@@ -11,3 +11,21 @@ def product_detail(request):
 
 def prueba(request, id):
     return HttpResponse(id)
+
+def agregar_residencia(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ResidenciaForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ResidenciaForm()
+
+    return render(request, 'agregar_residencia.html', {'form': form})
