67d2919d1f1e53175035e74109e4ed95cf4cc52f
Manu <38946479+manurtinez@users.noreply.github.com>
2019-05-06 12:39:25 -0300

    arranco agregar residencia

--- HWH/HomeSwitchHome/apps/residencia/forms.py
+++ HWH/HomeSwitchHome/apps/residencia/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class ResidenciaForm(forms.Form):
     nombre = forms.CharField(max_length=50)
-    capacidad = forms.IntegerField(null=True, blank=True)
+    capacidad = forms.IntegerField()
--- HWH/HomeSwitchHome/apps/residencia/views.py
+++ HWH/HomeSwitchHome/apps/residencia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Residencia
+from .forms import ResidenciaForm
 # Create your views here.
 
 def product_detail(request,id):
@@ -13,22 +14,8 @@ def prueba(request, id):
     return HttpResponse(id)
 
 def agregar_residencia(request):
-    # # if this is a POST request we need to process the form data
-    # if request.method == 'POST':
-    #     # create a form instance and populate it with data from the request:
-    #     form = ResidenciaForm(request.POST)
-    #     # check whether it's valid:
-    #     if form.is_valid():
-    #         # process the data in form.cleaned_data as required
-    #         # ...
-    #         # redirect to a new URL:
-    #         return HttpResponseRedirect('/thanks/')
-
-    # # if a GET (or any other method) we'll create a blank form
-    # else:
-    #     form = ResidenciaForm()
-
-    return render(request, 'agregar_residencia.html')
+   form = ResidenciaForm()
+   return render(request, 'agregar_residencia.html', {'form': form})
 
 def listado_residencias(request):
     residencia=Residencia.objects.get(auto_id=1)
