c281ac1ec262f79e398bd7c7b40b668cd7ef97cf
Manu <38946479+manurtinez@users.noreply.github.com>
2019-05-13 12:59:30 -0300

    hago 2 pavadas

--- HWH/HomeSwitchHome/HomeSwitchHome/urls.py
+++ HWH/HomeSwitchHome/HomeSwitchHome/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.residencia.urls')),
-    path('accounts/', include('apps.usuarios.urls'))
+    path('accounts/', include('apps.usuarios.urls')),
+    path('login/',LoginView.as_view(template_name='login.html'),name='login')
 ]
